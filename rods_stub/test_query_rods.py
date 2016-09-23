062c3c2be46b26d56ef4ebab6b7367371a70ab9f
James Hetherington <jamespjh@gmail.com>
2016-09-23 10:47:39 +0100

    Remove the rods stub

--- rods_stub/test_query_rods.py
+++
@@ -1,8 +0,0 @@
-from query_rods import *
-from itertools import islice
-
-def test_rod_manager():
-    rod_manager= TDAIRodsManager()
-    assert(len(rod_manager)) == 75503
-    top = str(rod_manager[52].next())
-    assert "GALENP" in top
