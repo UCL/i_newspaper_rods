3066b613a35ee3615a400e95b5f98011d8b4ad1b
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-11-08 13:06:40 +0000

    Added a quad sum.

--- newsrods/harness/utils.py
+++ newsrods/harness/utils.py
@@ -17,6 +17,9 @@ def normalize(word):
 def add(x,y):
     return x+y
 
+def quad_sum(a,b):
+    return [a[0]+b[0],a[1]+b[1],a[2]+b[2],a[3]+b[3]]
+
 def triple_sum(a,b):
     return [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
 
