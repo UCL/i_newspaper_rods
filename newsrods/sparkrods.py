26a39bdd15b88aa45511d6ae8279b27820c70742
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2017-08-08 15:18:27 +0100

    Test manually doing the round robin allocation to WOS nodes

--- newsrods/sparkrods.py
+++ newsrods/sparkrods.py
@@ -2,23 +2,25 @@
 Module to load and read the files using spark
 '''
 
+from itertools import cycle
 from newsrods.issue import Issue
 from requests import get
 
 DATA_STORE_URL = "utilities.rd.ucl.ac.uk"
+WOS_IP_BASE = '10.10.200.'
+WOS_IP_RANGE = range(70, 82)
 
 
-def get_streams(context, downsample=1, source="oids.txt"):
+def get_streams(context, source="oids.txt"):
     '''
     Turn a list of oids in a file into a RDD of Issues
     '''
     oids = [oid.strip() for oid in list(open(source))]
+    wos_ips = [WOS_IP_BASE + str(ip) for ip in WOS_IP_RANGE]
+    url_parts = zip(cycle(wos_ips), oids)
 
-    rddoids = context.parallelize(oids)
-    if downsample < 1:
-        rddoids = rddoids.sample(False, 1.0 / downsample)
-
-    issues = rddoids.map(lambda oid: 'http://' + DATA_STORE_URL +
+    rddoids = context.parallelize(url_parts)
+    issues = rddoids.map(lambda (ip, oid): 'http://' + ip +
                          '/objects/' + oid) \
                     .map(lambda url: get(url, stream=True)) \
                     .map(lambda stream: Issue(stream.raw))
