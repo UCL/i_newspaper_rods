44cfc0de4e0a968ab44aea14f855cfafe8cdaae2
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2017-08-11 11:16:57 +0100

    Use the utilities server to get the data as you are supposed to

--- newsrods/sparkrods.py
+++ newsrods/sparkrods.py
@@ -2,13 +2,10 @@
 Module to load and read the files using spark
 '''
 
-from itertools import cycle
 from newsrods.issue import Issue
 from requests import get
 
 DATA_STORE_URL = "utilities.rd.ucl.ac.uk"
-WOS_IP_BASE = '10.10.200.'
-WOS_IP_RANGE = range(70, 82)
 
 
 def get_streams(context, source="oids.txt"):
@@ -16,11 +13,9 @@ def get_streams(context, source="oids.txt"):
     Turn a list of oids in a file into a RDD of Issues
     '''
     oids = [oid.strip() for oid in list(open(source))]
-    wos_ips = [WOS_IP_BASE + str(ip) for ip in WOS_IP_RANGE]
-    url_parts = zip(cycle(wos_ips), oids)
 
-    rddoids = context.parallelize(url_parts)
-    issues = rddoids.map(lambda (ip, oid): 'http://' + ip +
+    rddoids = context.parallelize(oids)
+    issues = rddoids.map(lambda (oid): 'http://' + DATA_STORE_URL +
                          '/objects/' + oid) \
                     .map(lambda url: get(url, stream=True)) \
                     .map(lambda stream: Issue(stream.raw))
