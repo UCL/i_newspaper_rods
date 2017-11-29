f025ad15952be37236be6a3193dd313840588e32
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2017-11-29 17:47:13 +1300

    Compute year totals

--- queries/articles_per_year.py
+++ queries/articles_per_year.py
@@ -1,31 +1,21 @@
 '''
-A module to query files word and article counts. This returns
-the word count per article for each issues (number of word counts
-is the number of articles)
+A module to query article counts. This returns
+the number of articles per year.
 '''
 
-from operator import concat
-
-
-def print_datetime(datetime):
-    '''
-    Format a datetime. Cannot use strftime because the dates can be before 1900
-    '''
-    return "%02d-%02d-%04d" % (datetime.day, datetime.month, datetime.year)
+from operator import add
 
 
 def do_query(issues, _):
     '''
-    Get the count of words by article by issue
+    Get the count of words by articles by year
     '''
     # For each issue map the date, the number of articles as a key,
     # and the value is each articles length
-    articles = issues.flatMap(lambda issue: [(print_datetime(issue.date),
-                                              [len(article.words)])
-                                             for
-                                             article in issue.articles])
-    # Now add sum the issue-word counts, and change the format for output
+    articles = issues.flatMap(lambda issue: [(issue.date.year,
+                                              len(issue.articles))])
+    # Now add sum the article counts
     interesting_by_year = articles \
-        .reduceByKey(concat) \
+        .reduceByKey(add) \
         .collect()
     return interesting_by_year
