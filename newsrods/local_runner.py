01a5e6992af9be39a7b68bf1242c3c6b1f44e14a
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2017-12-04 16:17:41 +1300

    Simplying runners

--- newsrods/local_runner.py
+++ newsrods/local_runner.py
@@ -5,12 +5,5 @@ A runner to run the analysis directly on on the local machine
 from newsrods.sparkrods import run
 
 
-def main():
-    '''
-    Link the file loading with the query
-    '''
-    run(str(1))
-
-
 if __name__ == "__main__":
-    main()
+    run(str(1))
--- newsrods/remote_runner.py
+++ newsrods/remote_runner.py
@@ -6,13 +6,5 @@ import os
 from newsrods.sparkrods import run
 
 
-def main():
-    '''
-    Link the file loading with the query
-    '''
-
-    run(os.environ['SGE_TASK_ID'])
-
-
 if __name__ == "__main__":
-    main()
+    run(os.environ['SGE_TASK_ID'])
