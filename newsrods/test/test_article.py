b0fa523ed60578e6e3b211f94c750f32131dbcda
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-05-18 16:11:02 +0100

    Cleaning tests

--- newsrods/test/test_article.py
+++ newsrods/test/test_article.py
@@ -1,29 +1,35 @@
-'''
+"""
 Tests for Article
-'''
+"""
 
-from unittest import TestCase
 import os
+from unittest import TestCase
 
 from ..issue import Issue
 
 
 class TestPage(TestCase):
-    '''
+    """
     Test class for Article
-    '''
+    """
 
     def setUp(self):
-        '''
+        """
         Load the standard test file
-        '''
+        """
         with open(os.path.join(os.path.dirname(__file__),
                                'fixtures', '2000_04_24.xml')) as fixture:
             issue = Issue(fixture)
         self.article = issue.articles[0]
 
     def test_words_in_article(self):
-        '''
+        """
         Check that the article length is correct
-        '''
+        """
         assert len(self.article.words) == 18
+
+    def test_ocr_quality(self):
+        """
+        Make sure that the ocr quality is read
+        """
+        assert self.article.quality == 0
