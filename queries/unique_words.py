d19abd2648c5565f74c713bbef43f64de9cda2f4
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-05-22 10:26:07 +0100

    Make unique words sample
    This stops it counting all words, and will hopefully make the results smaller as it loses the long tail.

--- queries/unique_words.py
+++ queries/unique_words.py
@@ -3,6 +3,10 @@ A module to query files to find every word used in the TDA and its frequency
 """
 
 from operator import add
+from random import uniform
+
+
+ACCEPTANCE_PROBABILTY = 0.2
 
 
 def do_query(issues, _in, _log):
@@ -17,5 +21,36 @@ def do_query(issues, _in, _log):
                                               word in article.words])
 
     # Now add sum the word counts
-    word_counts = words.reduceByKey(add).collect()
+    word_counts = words. \
+        reduceByKey(add). \
+        map(fix_counts). \
+        collect()
     return word_counts
+
+
+def fix_counts(record):
+    """
+    Fix the record counts based on the acceptance
+    probability
+    """
+    word, count = record
+    factor = 1.0 / ACCEPTANCE_PROBABILTY
+    return (word, int(count * factor))
+
+
+def count_words_in_article(words):
+    """
+    Count the number of times each word appears.
+    To deal with the long tail of unique words, roll a dice
+    before allowing a word in
+    """
+    counts = {}
+    for word in words:
+        if uniform(0, 1) > ACCEPTANCE_PROBABILTY:
+            continue
+        count = counts.get(word)
+        if count is None:
+            counts[word] = 1
+        else:
+            counts[word] = count + 1
+    return [(word, count) for word, count in counts.items()]
